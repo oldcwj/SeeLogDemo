9fcb96b13c737b04e9686c1a5653787c9b829592
cuiwj <oldcwj@gmail.com>
2017-02-23 11:10:28 +0800

    init

---
+++ app/src/main/java/seelog/oldcwj/com/seelogdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package seelog.oldcwj.com.seelogdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
